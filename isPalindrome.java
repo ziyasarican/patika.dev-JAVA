b928238c8ed9b952e4324cbf36e1594c9b33b24a
ziyasarican <87414202+ziyasarican@users.noreply.github.com>
2021-12-27 22:48:32 +0300

    Update isPalindrome.java

--- isPalindrome.java
+++ isPalindrome.java
@@ -2,18 +2,22 @@ import java.util.Scanner;
 
 public class Main {
 
-    static int isPalindrome(int number){
+    static void isPalindrome(int number){
         int temp = number, reverseNumber = 0, lastDigit;
         while(temp != 0){
             lastDigit = temp % 10;
             reverseNumber = (reverseNumber * 10) + lastDigit;
             temp /= 10;
         }
-        return reverseNumber;
+        if(reverseNumber == number)
+            System.out.println(number + " is palindrome");
+        else
+            System.out.println(number + " is not palindrome");
     }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int number = input.nextInt();
-        System.out.println(isPalindrome(number));
+
+        isPalindrome(number);
     }
 }
