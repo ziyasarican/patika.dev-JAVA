87fb4f89954f2cd2ed99ba9a6e9127feb93f50c1
ziyasarican <87414202+ziyasarican@users.noreply.github.com>
2022-01-02 14:35:13 +0300

    Create Main.java

---
+++ salaryCalculator/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Employee employee1 = new Employee("Ali",2000, 45,1985);
+        employee1.printInfo();
+
+    }
+}
