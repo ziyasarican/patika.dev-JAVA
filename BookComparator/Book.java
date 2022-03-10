b7610c346e6f7a9fd2df03a1ef9ff0cd91f25641
ziyasarican <87414202+ziyasarican@users.noreply.github.com>
2022-03-10 23:35:45 +0300

    Create Book.java

---
+++ BookComparator/Book.java
@@ -0,0 +1,54 @@
+import java.util.Comparator;
+
+public class Book {
+    private String bookName;
+    private int page;
+    private String author;
+    private int releaseDate;
+
+    public Book(String bookName, int page, String author, int releaseDate) {
+        this.bookName = bookName;
+        this.page = page;
+        this.author = author;
+        this.releaseDate = releaseDate;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = Integer.parseInt(releaseDate);
+    }
+
+    @Override
+    public String toString(){
+        return "Book name: " + this.bookName + " Pages: " + this.page + " Author: " + this.author;
+    }
+
+
+}
